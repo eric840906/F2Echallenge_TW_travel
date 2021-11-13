1b9cccb3754f788d7afd7b3083e545a512d86f6b
Eric Chiu <eric840906@hotmail.com.tw>
2021-11-13 11:37:24 +0800

    compress static images

--- src/data/restaurantCategories.js
+++ src/data/restaurantCategories.js
@@ -1,44 +1,47 @@
+import local from '../assets/images/food/bubbletea.jpg'
+import foreign from '../assets/images/food/nasilemak.jpg'
+import chinese from '../assets/images/food/bun.jpg'
+import nightmarket from '../assets/images/food/rice.jpg'
+import snack from '../assets/images/food/ice.jpg'
+import pineapplecake from '../assets/images/food/cake.jpg'
+import veg from '../assets/images/food/veg.jpg'
+import bbq from '../assets/images/food/bbq.jpg'
+import other from '../assets/images/food/other.jpg'
 export const restaurantCategories = [
   {
     name: '地方特產',
-    imageUrl:
-      'https://image.cache.storm.mg/styles/smg-800x533-fp/s3/media/image/2017/06/19/20170619-065353_U3260_M291934_836b.jpg?itok=lPYUVYdG'
+    imageUrl: local
   },
   {
     name: '異國料理',
-    imageUrl:
-      'https://i0.wp.com/chiubistro.com/wp-content/uploads/2021/02/nasi-lemak-coconut-pandan-rice-3030254-step-07-ccbeb81d2b754d9c899b2edfaf386bc9.jpg?resize=600%2C600&ssl=1'
+    imageUrl: foreign
   },
   {
     name: '中式美食',
-    imageUrl:
-      'https://s.yimg.com/os/zh-Hant-TW/homerun/tw-travel.tripass_news.com.tw/f1f6b909b80db69af84aa312cdc9aa49'
+    imageUrl: chinese
   },
   {
     name: '夜市小吃',
-    imageUrl:
-      'https://doqvf81n9htmm.cloudfront.net/data/annahu_151/180913/1920_shutterstock_1138941431.jpg'
+    imageUrl: nightmarket
   },
   {
     name: '甜點冰品',
-    imageUrl:
-      'https://taiwan.sharelife.tw/tw-feat-store-img/53730/8709190320100517.jpg'
+    imageUrl: snack
   },
   {
     name: '伴手禮',
-    imageUrl: 'https://cdn2.ettoday.net/images/1446/1446741.jpg'
+    imageUrl: pineapplecake
   },
   {
     name: '素食',
-    imageUrl: 'https://www.1111boss.com.tw/news/images/IMG_Winner13201_1.jpg'
+    imageUrl: veg
   },
   {
     name: '火烤料理',
-    imageUrl:
-      'https://www.foodnext.net/dispPageBox/getFile/GetImg.aspx?FileLocation=%2FPJ-FOODNEXT%2FFiles%2F&FileName=photo-22754-i.jpg'
+    imageUrl: bbq
   },
   {
     name: '其他',
-    imageUrl: 'http://s2.lookerideas.com/imgs/201807/17/4/15318149755421.png'
+    imageUrl: other
   }
 ]
